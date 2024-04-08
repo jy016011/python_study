8a092e904ba9fa7b843cc3b22c31288bb7c2b7e9
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-04-08 19:30:47 +0900

    240408 django study - manage model in django shell - add recently published boolean check function in question model - add __str__ in choice model

--- django_projects/practice/polls/models.py
+++ django_projects/practice/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # 모델 생성
@@ -7,11 +10,21 @@ from django.db import models
 
 class Question(models.Model):
     question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()- datetime.timedelta(days=1)
+
     def __str__(self):
-        return "제목: {}, 날짜: {}".format(self.question, self.pub_date)
+        if self.was_published_recently():
+            new_badge = 'NEW!'
+        else:
+            new_badge = ''
+        return "{} 제목: {}, 날짜: {}".format(new_badge, self.question, self.pub_date)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return "[{}] {}".format(self.question.question, self.choice_text)
