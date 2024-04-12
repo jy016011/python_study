b586d1c9f5a72367e849cab5801a3aa5a44faf70
jy016011 <39334789+jy016011@users.noreply.github.com>
2024-04-13 00:33:54 +0900

    240413 django study add QuestionList API view test cases

--- django_projects/practice/polls_api/tests.py
+++ django_projects/practice/polls_api/tests.py
@@ -2,7 +2,42 @@ from django.test import TestCase
 from polls_api.serializers import QuestionSerializer, VoteSerializer
 from django.contrib.auth.models import User
 from polls.models import Question, Choice, Vote
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.utils import timezone
 # Create your tests here.
+
+class QuestionListTest(APITestCase):
+    def setUp(self) -> None:
+        self.question_data = {'question': 'some question'}
+        # method 안에서 사용할 때에는 reverser_lazy 말고 reverse 사용
+        self.url = reverse('question-list')
+
+    def test_case_create_question(self):
+        user = User.objects.create(username='testuser', password='testpass')
+        # 사용자를 강제로 로그인하는 기능
+        self.client.force_authenticate(user=user)
+        response = self.client.post(self.url, self.question_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['question'], self.question_data['question'])
+        self.assertEqual(Question.objects.count(), 1)
+        question = Question.objects.first()
+        self.assertEqual(question.question, self.question_data['question'])
+        self.assertLess((timezone.now() - question.pub_date).total_seconds(), 1)
+
+    def test_create_without_authentication(self):
+        response = self.client.post(self.url, self.question_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_questions(self):
+        question = Question.objects.create(question='question1')
+        choice = Choice.objects.create(question=question, choice_text='1')
+        Question.objects.create(question='question2')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['choices'][0]['choice_text'], choice.choice_text)
 
 class VoteSerializerTest(TestCase):
     def setUp(self) -> None:
